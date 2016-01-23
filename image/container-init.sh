090e34ee50b691c1974dc54f4bcaeb23b352171f
Ivan Vlatkovic <ivan.vlatkovic@gmail.com>
2016-01-23 20:32:49 +0100

    properly quote variables in if statements

--- image/container-init.sh
+++ image/container-init.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-if [[ $1=='split' ]]; then
+if [[ "$1" == 'split' ]]; then
   shift
   exec ffmpeg-split.sh "$@"
-elif [[ $1=='ffmpeg' ]]; then
+elif [[ "$1" == 'ffmpeg' ]]; then
   shift
   exec ffmpeg "$@"
-elif [[ $1=='ffprobe' ]]; then
+elif [[ "$1" == 'ffprobe' ]]; then
   shift
   exec ffprobe "$@"
 fi
